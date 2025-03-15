38ddd9e3d8a94ce7c08f0f18dc31f546d4e0ac61
21bcs11410 <gauravb965516@gmail.com>
2025-03-15 23:29:15 +0530

    Complete Home Page UI

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -25,6 +25,7 @@ const Community = ["Forums", "Chapters", "Events"];
 const Footer = () => {
   return (
     <div className="bg-richblack-800">
+
       <div className="flex lg:flex-row gap-8 items-center justify-between w-11/12 max-w-maxContent text-richblack-400 leading-6 mx-auto relative py-14">
         <div className="border-b w-[100%] flex flex-col lg:flex-row pb-5 border-richblack-700">
           {/* Section 1 */}
@@ -175,6 +176,7 @@ const Footer = () => {
           <div className="text-center">Made with ❤️ by Gaurav</div>
         </div>
       </div>
+
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,9 @@ import { CTAButton } from '../components/core/HomePage/Button';
 import Banner from "../assets/Images/banner.mp4";
 import { CodeBlocks } from '../components/core/HomePage/CodeBlocks';
 import Footer from '../components/common/Footer';
-
+import { TimelineSection } from '../components/core/HomePage/TimelineSection';
+import { LearningLanguageSection } from '../components/core/HomePage/LearningLanguageSection';
+import InstructorSection from '../components/core/HomePage/InstructorSection';
 
 export const Home = () => {
   return (
@@ -121,11 +123,77 @@ export const Home = () => {
 
 
         {/* section 2 */}
-        {/* //here to add section 2 */}
+        <div className='bg-pure-greys-5 text-richblack-700'>
+            
+            <div className='homepage_bg h-[310px]'>
 
+                <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto'>
+
+                    <div className='h-[150px]'></div>
+
+                    <div className='flex flex-row text-white gap-7'>
+
+                        <CTAButton active={true} linkto={"/signup"}>
+                            <div className='flex items-center gap-3'>
+                                Explore Full Catalog
+                                <FaArrowRight/>
+                            </div>
+                        </CTAButton>
+
+                        <CTAButton active={false} linkto={"/signup"}>
+                            <div>
+                                Learn More
+                            </div>
+                        </CTAButton>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className='mx-auto gap-5 w-11/12 max-w-maxContent flex flex-col items-center justify-between'>
+
+                <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
+
+                    <div className='text-4xl font-semibold w-[45%]'>
+                        Get the skills you need for a 
+                        <HighLightText text={"Job that is in demand"}/>
+                    </div>
+
+                    <div className='flex flex-col gap-10 w-[40%] items-start'>
+
+                        <div className='text-[16px]'>
+                            The modern StudyNotion is the dictates its own terms. Today, to be a competitive specialist requires more than professional skills.
+                        </div>
+
+                        <CTAButton active={true} linkto={"/signup"}>
+                            Learn More
+                        </CTAButton>
+
+                    </div>
+
+                </div>
+
+                <TimelineSection/>
+
+                <LearningLanguageSection/>
+
+            </div>
+
+        </div>
 
 
         {/* section 3 */}
+        <div className='w-11/12 mx-auto max-w-maxContent mb-14 flex-col items-center justify-between gap-8 first-letter bg-richblack-900 text-white'>
+
+            <InstructorSection />
+
+            {/* <h2 className='text-center text-4xl font-semobold mt-10'>review from Other Learners</h2> */}
+      </div>
+
+
+
 
 
 
